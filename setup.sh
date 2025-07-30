307a9e73731f4da3eeecf7aada54734510e41e83
2SSK <100ravsinghkarmwar@gmail.com>
2025-07-30 22:13:50 +0530

    chore(inventory): add server IP to [server] group
    chore(playbook.yml): set environment variables for UTF-8 support
    
    refactor(setup.sh): remove user checks and simplify Ansible installation process

--- setup.sh
+++ setup.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [[ "$EUID" -ne 0 ]]; then
-    echo "Please run this script as root (e.g., using sudo)"
-    exit 1
-fi
-
-# Parameterize the username (default to "ssk" if not provided)
-USER_TO_ADD="${1:-ssk}"
-
-# Check if user exists
-if ! id "$USER_TO_ADD" &>/dev/null; then
-    echo "User '$USER_TO_ADD' does not exist."
-    exit 1
-fi
-
-# Add user to wheel group if not already
-if ! groups "$USER_TO_ADD" | grep -qw "wheel"; then
-    echo "Adding $USER_TO_ADD to wheel group (sudoers)..."
-    usermod -aG wheel "$USER_TO_ADD"
-
-    # Ensure wheel group has sudo privileges
-    sed -i '/^# %wheel ALL=(ALL:ALL) ALL/s/^# //' /etc/sudoers
-fi
-
 # Check if pacman is present
 if ! command -v pacman >/dev/null 2>&1; then
     echo "This script is for Arch-based systems only."
@@ -46,7 +22,7 @@ fi
 
 # Install ansible using yay
 if ! command -v ansible >/dev/null 2>&1; then
-    sudo -u "$USER_TO_ADD" yay -S --noconfirm ansible
+    sudo  yay -S --noconfirm ansible
 else
     echo "Ansible is already installed."
 fi
